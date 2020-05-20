36a5c3548c2fa803c748675a4ca0155e265626bf
Madhusudhan <madhusudhan.shagam@gmail.com>
2020-05-20 03:28:21 -0700

    master latest 1 commit

--- test_project/canvas/frontend/src/components/Assignments.js
+++ test_project/canvas/frontend/src/components/Assignments.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios';
 <<<<<<< HEAD
 import './Assignments.css'`
+import '';
 import './Announcements.css'
 import './Assignments.js'
 =======
